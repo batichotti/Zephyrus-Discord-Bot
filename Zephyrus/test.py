839f07001d5c7cecec85fd60def7343ab1c4ea74
Mateus <matebatichotti@gmail.com>
2021-09-26 18:05:53 -0300

    Update test.py

--- Zephyrus/test.py
+++ Zephyrus/test.py
@@ -1,8 +1,11 @@
 import discord
 from discord.ext import commands, tasks
 import datetime
+import requests
+from bs4 import BeautifulSoup as bs
+from requests.models import Response
 
-bot = commands.Bot("$")
+bot = commands.Bot("\\")
 
 @bot.event
 async def on_ready():
@@ -34,11 +37,43 @@ async def calculate_expression(ctx, *expression):
     response = eval(expression)
     await ctx.send(f"A resposta é {response}")
 
-@bot.command()
+@bot.command(name="crypto")
 async def binance(ctx, coin, base):
-    pass
+    try:
+        response = requests.get(f"https://api.binance.com/api/v3/ticker/price?symbol={coin.upper()}{base.upper()}")
+        data = response.json()
+        price = data.get("price")
+        if price:
+            await ctx.send(f"O valor do par {coin}/{base} é {round(price, 2)}")
+        else:
+            await ctx.send(f"O par {coin}/{base} é invalido")
+    except Exception as e:
+        await ctx.send("Ops... Erro detectado!")
+        print(e)
 
-@tasks.loop(minutes=60)
+@bot.command(name="dólar")
+async def dollar(ctx):
+    try:
+        url = requests.get('https://www.remessaonline.com.br/cotacao/cotacao-dolar')
+        soup = bs(url.text, 'html.parser')
+        dolar = soup.find('div', {'class': 'style__Text-sc-15flwue-2 cSuXFv'}).text[0:4]
+        await ctx.send(f"Um dólar custa R${dolar} ")
+    except Exception as e:
+        await ctx.send("Ops... Erro detectado!")
+        print(e)
+
+@bot.command()
+async def euro(ctx):
+    try:
+        url = requests.get('https://www.remessaonline.com.br/cotacao/cotacao-euro')
+        soup = bs(url.text, 'html.parser')
+        euro = soup.find('div', {'class': 'style__Text-sc-15flwue-2 cSuXFv'}).text[0:4]
+        await ctx.send(f"Um euro custa R${euro} ")
+    except Exception as e:
+        await ctx.send("Ops... Erro detectado!")
+        print(e)
+
+@tasks.loop(hours=1)
 async def current_time():
     now = datetime.datetime.now()
     now = now.strftime("%d/%m/%Y às %H:%M:%S")
