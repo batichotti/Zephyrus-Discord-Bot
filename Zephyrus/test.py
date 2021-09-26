58d2ec9dcd6986d6383216d9525b8204c9e028de
Mateus <matebatichotti@gmail.com>
2021-09-26 15:21:29 -0300

    Update test.py

--- Zephyrus/test.py
+++ Zephyrus/test.py
@@ -1,5 +1,6 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+import datetime
 
 bot = commands.Bot("$")
 
@@ -7,6 +8,7 @@ bot = commands.Bot("$")
 async def on_ready():
     print(f'Estamos conectados como {bot.user}')
     print('>' + '-'*34 + '<')
+    current_time.start()
 
 @bot.event
 async def on_message(message):
@@ -25,5 +27,22 @@ async def send_hello(ctx):
     name = ctx.author.name
     response = f"Olá, {name}!"
     await ctx.send(response)
+
+@bot.command(name="calcular")
+async def calculate_expression(ctx, *expression):
+    expression = ''.join(expression)
+    response = eval(expression)
+    await ctx.send(f"A resposta é {response}")
+
+@bot.command()
+async def binance(ctx, coin, base):
+    pass
+
+@tasks.loop(minutes=60)
+async def current_time():
+    now = datetime.datetime.now()
+    now = now.strftime("%d/%m/%Y às %H:%M:%S")
+    channel = bot.get_channel(891426585419325470)
+    await channel.send(f"Data atual: {now}")
 
 bot.run('ODkxMzUxMTQxNjk3MzUxNzQw.YU9FcA.fSckrOmA0fl2-6aZwjz5h2W8DvE')
