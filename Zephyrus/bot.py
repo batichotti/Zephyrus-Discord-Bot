2430099b0f9d488b92346e833712c01e1a75262c
Mateus <matebatichotti@gmail.com>
2021-10-07 13:47:29 -0300

    ,

--- Zephyrus/bot.py
+++ Zephyrus/bot.py
@@ -12,7 +12,6 @@ def load_cogs(bot):
         if file.endswith(".py"):
             cog = file[:-3]
             bot.load_extension(f"commands.{cog}")
-            print(f"{cog}")
 
 
 load_cogs(bot)
